bac15550d1b4929c14694963141e36a2c21b0442
Amy Truong <amytruong@github.com>
2015-09-08 13:20:45 -0700

    test

---
+++ src/hamburgers rule/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hamburgers_rule
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+        }
+    }
+}
